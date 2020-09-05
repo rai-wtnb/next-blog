ea0e351bf6493a4e230f58af3c982de18824265b
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2020-09-06 08:23:26 +0900

    feat: implemented responsive design

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang='ja'>
         <Head>
+          <link rel='icon' href='/favicon.ico' />
           <link
             rel='stylesheet'
             href='https://fonts.googleapis.com/css2?family=Sawarabi+Gothic&display=swap'
