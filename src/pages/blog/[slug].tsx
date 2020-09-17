d596d681f7ff8bfbbdadb630c09d7e98293f7df1
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2020-09-18 08:45:16 +0900

    fix: fixed openGraph

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NextPage, GetStaticProps, GetStaticPaths } from 'next';
-import Head from 'next/head';
 
 import { Layout } from '../../components/layout';
 import { BlogDetail } from '../../components/blog';
