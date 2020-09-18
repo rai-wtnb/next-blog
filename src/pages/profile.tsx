a31919435e58033cd05b367dded0a690ac8a3eac
rai-watanabe <55418247+rai-wtnb@users.noreply.github.com>
2020-09-18 10:31:33 +0900

    fix: change share png (#26)
    * feat/modified next-seo and og:image
    
    * fix: fixed bug
    
    * fixed metatag
    
    * fix: fixed openGraph
    
    * fix: changed share.png
    
    * fix: fixed bug
    
    * fixed metatag
    
    * fix: fixed openGraph
    
    * fix: changed share.tsx
    
    * fix: fixed profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NextPage } from 'next';
+import { H1 } from '../styles/globalStyle';
+
+// プロフィール作成予定
+
+const ProfilePage: NextPage = () => {
+  return (
+    <H1>プロフィール作成中...</H1>
+  )
+}
+
+export default ProfilePage;
